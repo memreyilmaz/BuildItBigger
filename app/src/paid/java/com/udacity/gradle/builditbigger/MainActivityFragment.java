845a4cbd36bf15dd13c03fc865e72f20a0327070
memreyilmaz <32981001+memreyilmaz@users.noreply.github.com>
2018-08-30 16:46:12 +0300

    Free and Paid Flavors Added

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -6,7 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 //import com.example.android.javajokes.Joker;
+
 /**
  * A placeholder fragment containing a simple view.
  */
